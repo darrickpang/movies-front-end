235276bc6b2f2590143012408c7dc65b76424a16
darrickpang <dkypang@yahoo.com>
2021-03-16 18:49:11 -0700

    user

---
+++ src/containers/UserMainContent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {  withRouter, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import Chemical from '../components/Chemical'
+
+class UserMainContent extends React.Component {
+    renderUserInfo = () => {
+        return (
+            <div className="student-info">
+                <h3>User name: {this.props.user.name}</h3>
+            </div>
+        )
+    }
+
+    renderLogout = () => {
+        return (
+            <Button className="button" onClick={() => {
+                localStorage.clear()
+                this.props.history.push('/')
+                }}>Log Out
+            </Button>
+        )
+    }
+
+    render(){
+        return(
+            <div className="main-page">
+                Welcome to your main page. 
+                {this.renderUserInfo()}
+                {this.renderLogout()}               
+            </div> 
+        )
+    }
+}
+
+export default withRouter(UserMainContent)
