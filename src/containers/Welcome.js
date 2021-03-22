c7b067a8f0b1da1a07b9351ef5381029925fa5d2
darrickpang <dkypang@yahoo.com>
2021-03-21 18:15:07 -0700

    more css

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -8,8 +8,8 @@ const Welcome = () => {
     return (
       <div style={{backgroundImage: "url(" + require("../image/movies.jpg") + ")",}} className="page-header">
         <Container className='welcome-page'>
-            <h1>Pool Chemicals</h1>
-            <h3>Record pool chemicals.</h3>
+            <h1>Movie Time</h1>
+            <h3>Find and discuss your movies.</h3>
             <br />
             <Button className="login-button" onClick={() => history.push('/user_signup')}> User Sign Up</Button>
             <Button className="login-button" onClick={() => history.push('/user_login')}>User Login</Button>
